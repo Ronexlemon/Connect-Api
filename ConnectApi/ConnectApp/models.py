b820a7dba42a03ebcdc198b2ac787446c3f789a8
RonexLemon <ronexondimu@gmail.com>
2022-06-27 13:35:55 +0300

    added the models

--- ConnectApi/ConnectApp/models.py
+++ ConnectApi/ConnectApp/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Products(models.Model):
+    productName = models.CharField(max_length=255)
+    productDescription = models.CharField(max_length=255)
+    productImageUrl = models.URLField(max_length=10000)
+    productPrice = models.FloatField(max_length=100)
+
+    def __str__(self):
+        return self.productName
+
+
