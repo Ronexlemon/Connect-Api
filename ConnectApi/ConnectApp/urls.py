5ef80f51762a8feaea1b361324cf75a434daeec4
RonexLemon <ronexondimu@gmail.com>
2022-06-28 09:29:56 +0300

    added the user interface for Api

--- ConnectApi/ConnectApp/urls.py
+++ ConnectApi/ConnectApp/urls.py
@@ -5,5 +5,9 @@ from rest_framework.routers import DefaultRouter
 route=DefaultRouter()
 route.register("products",views.ProductsView)
 urlpatterns = [
-    path("",include(route.urls))]
+    path("api/",include(route.urls)),
+path("",views.index,name="home"),
+path("home/form/",views.form,name="form"),
+path("home/form/addproduct/",views.addproduct,name="addproduct")]
+
 
