ad3deecdd4c62ae5448bdb97211a34122bd32cf6
RonexLemon <ronexondimu@gmail.com>
2022-06-29 15:34:40 +0300

    modify the setting

--- ConnectApi/settings.py
+++ ConnectApi/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY =os.getenv("SECRET_KEY") #'django-insecure-bghy%-=)dd05)wv+7#q+gydjj(#mrg!no(g-e+)k8ody#hs+1f'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["localhost"]
 
