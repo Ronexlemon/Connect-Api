80229264b9caa64cd614ef27a09bd09b874ed48b
RonexLemon <ronexondimu@gmail.com>
2022-06-29 07:08:26 +0300

    change path to get os

--- ConnectApi/settings.py
+++ ConnectApi/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY =os.environ.get("SECRET_KEY") #'django-insecure-bghy%-=)dd05)wv+7#q+gydjj(#mrg!no(g-e+)k8ody#hs+1f'
+SECRET_KEY =os.getenv("SECRET_KEY") #'django-insecure-bghy%-=)dd05)wv+7#q+gydjj(#mrg!no(g-e+)k8ody#hs+1f'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
